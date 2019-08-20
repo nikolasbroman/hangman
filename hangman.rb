a6b1951ad58c054282e640e6e2f204286d99e9f1
Nikolas Broman <nikolas.broman@gmail.com>
2019-08-20 06:34:19 +0300

    Add methods 'get_guess' and 'check_guess'

--- hangman.rb
+++ hangman.rb
@@ -16,7 +16,7 @@ class Hangman
   def new_game
     set_secret_word
     set_secret_word_with_underscores
-    #get guess, check input
+    guess = get_guess
     #check whether guess is correct or incorrect -> add to array
     #if correct, update @secret_word_with_underscores
     #check if win
@@ -24,15 +24,33 @@ class Hangman
 
   def set_secret_word
     word_list = File.readlines("5desk.txt").map(&:chomp)
-    secret_word = ""
-    until secret_word.length.between?(5,12) && secret_word[0] =~ /[a-z]/
-      secret_word = word_list.sample
+    while secret_word = word_list.sample
+      break if secret_word.length.between?(5,12) && secret_word[0] =~ /[a-z]/
     end
     @secret_word = secret_word
   end
 
   def set_secret_word_with_underscores
     @secret_word_with_underscores = ("_" * @secret_word.length).split("").join(" ")
+  end
+
+  def get_guess
+    puts "Next guess:"
+    while guess = gets.chomp.downcase
+      return guess if check_guess(guess)
+    end
+  end
+
+  def check_guess(guess)
+    if @correct_guesses.include?(guess) || @incorrect_guesses.include?(guess)
+      puts "You have already guessed '#{guess}'. Try a new guess:"
+      false
+    elsif guess.length != 1 && guess.length != @secret_word.length
+      puts "Your guess should be 1 letter or the entire word (#{@secret_word.length} lettters):"
+      false
+    else
+      true
+    end
   end
 
 end
